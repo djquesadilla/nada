b6d3fbdf62dbe0ba69b31530a6cd3143421911fd
Fábio Costa <fabio@mundoquesadilla.com>
2022-08-25 22:47:04 +0100

    Pages are turned...

--- src/Nada.js
+++ src/Nada.js
@@ -1,27 +1,38 @@
+import React from 'react';
 import './Nada.css';
 
 const story = ["First page of the book", "Second page of nada happening", "And third one is even Nadaer"]
 
-function StoryText() {
-  return (
-    <div className='story-text'>{story[0]}</div>
-  )
+class StoryText extends React.Component {
+  render() {
+    return <div className='story-text'>{this.props.storyText}</div>;
+  }
 }
 
-function NextButton() {
-  return  (
-    <button class="btn">Next</button>
-  )
-}
-
-function Nada() {
-  return (
-    <div className="Nada">
-      <StoryText />
-      <NextButton />
-    </div>
+class Nada extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { storyIndex: 0 };
+    // This binding is necessary to make `this` work in the callback
     
-  );
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    console.log(this.state)
+    this.setState(
+      prevState => ({
+        storyIndex: prevState.storyIndex + 1
+      }));
+  }
+
+  render() {
+    console.log(this.state.storyIndex)
+    return <div className="Nada">
+    <StoryText storyText={story[this.state.storyIndex]}/>
+    <button className="btn" onClick={this.handleClick}>Next</button>
+  </div>;
+  }
 }
 
 export default Nada;
