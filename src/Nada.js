b2db95bff93496499728d64a923d44478765e370
Fábio Costa <fabio@mundoquesadilla.com>
2022-09-12 14:49:55 +0100

    reorganized code

--- src/Nada.js
+++ src/Nada.js
@@ -1,62 +1,7 @@
 import React from 'react';
 import BackgroundMusic from './BackgroundMusic';
-import story from './story/story'
+import NadaStory from './gameComponents/NadaStory';
 import './Nada.css';
-
-class StoryText extends React.Component {
-  render() {
-    return <div className={this.props.fadeOut ? 'story-text fade-out-text': 'story-text fade-in-text'}>{this.props.storyText}</div>;
-  }
-}
-
-class NadaStory extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { 
-      storyIndex: 0,
-      fadeOut: false,
-      ready: true
-    };
-    // This binding is necessary to make `this` work in the callback
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick() {
-    // set timer of 10 seconds for:
-    // * fade-out / disable button
-    // * fade-in / enable button again
-
-    this.setState(
-      prevState => ({
-        fadeOut: true,
-        ready: false
-      }));
-
-    setTimeout(() => {
-      this.setState(
-        prevState => ({
-          storyIndex: prevState.storyIndex + 1,
-          fadeOut: false
-        })
-      )
-    }, 5000)
-
-    setTimeout(() => {
-      this.setState(
-        prevState => ({
-          ready: true
-        })
-      )
-    }, 8000)
-  }
-
-  render() {
-    return <div className='nada-story'>
-      <StoryText storyText={story[this.state.storyIndex]} fadeOut={this.state.fadeOut} />
-      <button className="next-button" disabled={this.state.ready ? false : true} onClick={this.handleClick}>Next</button>
-    </div>;
-  }
-}
 
 class Nada extends React.Component {
   render() {
